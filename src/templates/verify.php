c997eaac217470d3d2f49ab51297372652f55ec4
Joe Dolson <design@joedolson.com>
2021-04-23 14:06:28 -0500

    Verification template and CSS

--- src/templates/verify.php
+++ src/templates/verify.php
@@ -14,7 +14,7 @@
 <head>
 	<title><?php bloginfo( 'blogname' ); ?> &bull; <?php _e( 'Verify Ticket', 'my-tickets' ); ?> &bull; <?php mt_ticket_id(); ?></title>
 	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-	<link type="text/css" rel="stylesheet" href="<?php echo plugins_url( 'css/generic.css', __FILE__ ); ?>"/>
+	<link type="text/css" rel="stylesheet" href="<?php echo plugins_url( 'css/generic.css', __FILE__ ); ?>" />
 </head>
 <body>
 <div class='panel verify <?php mt_ticket_method(); ?>'>
@@ -23,7 +23,7 @@
 
 	<p><?php mt_event_date(); ?> @ <span class='time'><?php mt_event_time(); ?></span></p>
 
-	<p class='mt-verification'><?php mt_verification(); ?></p>
+	<div class='mt-verification'><?php mt_verification(); ?></div>
 
 </div>
 
