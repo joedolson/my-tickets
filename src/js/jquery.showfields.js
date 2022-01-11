a56963346d037bcb33b1342c7239bd49f527f416
Joe Dolson <design@joedolson.com>
2022-01-10 19:21:08 -0600

    Allow general admission to submit with empty dates/availability
    See #12

--- src/js/jquery.showfields.js
+++ src/js/jquery.showfields.js
@@ -4,15 +4,19 @@ jQuery(document).ready(function ($) {
 		$('.mt-ticket-form').hide();
 	}
 	var general_status = $('input[name=mt_general]:checked').val();
-	console.log( general_status );
 	if (general_status !== 'general') {
+		$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
+		$('.mt-available-tickets input').attr( 'required', 'required' ).attr( 'aria-required', 'true' );
 		$('.mt-ticket-validity').hide();
 	} else {
+		$('.mt-ticket-dates input').removeAttr('required').removeAttr('aria-required');
+		$('.mt-available-tickets input').removeAttr( 'required' ).removeAttr( 'aria-required' );
 		$('.mt-ticket-dates').hide();
 	}
 	$('.mt-trigger').on( 'click', function() {
 		var checked_status = $(this).prop('checked');
-		if (checked_status == true) {
+
+		if (checked_status == true ) {
 			$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
 			$('.mt-available-tickets input').attr( 'required', 'required' ).attr( 'aria-required', 'true' );
 			$('.mt-ticket-form').show(300);
@@ -24,12 +28,15 @@ jQuery(document).ready(function ($) {
 	});
 	$('input[name=mt_general]').on( 'change', function() {
 		var checked_status = $('input[name=mt_general]:checked').val();
-		console.log( 'change', checked_status );
 		if (checked_status == 'general' ) {
 			$('.mt-ticket-dates' ).hide();
+			$('.mt-ticket-dates input').removeAttr('required').removeAttr('aria-required');
+			$('.mt-available-tickets input').removeAttr( 'required' ).removeAttr( 'aria-required' );
 			$('.mt-ticket-validity').show();
 		} else {
 			$('.mt-ticket-dates' ).show();
+			$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-available-tickets input').attr( 'required', 'required' ).attr( 'aria-required', 'true' );
 			$('.mt-ticket-validity').hide();
 		}
 	});
