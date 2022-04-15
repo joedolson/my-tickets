f09ae3354a95266d3f47f39a2ce2500f98f6f7e2
Joe Dolson <design@joedolson.com>
2022-04-15 11:11:31 -0500

    Checkbox event only added if already enabled.
    Also: set admissions type on activation.

--- src/js/jquery.showfields.js
+++ src/js/jquery.showfields.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function ($) {
-	var initial_status = $('.mt-trigger').prop('checked');
+	var initial_status    = $('.mt-trigger').prop('checked');
+	var admissionType     = $('input[name=mt_general]');
+	var admissionSelected = false;
+	admissionType.each( function() {
+		admissionSelected = $( this ).prop('checked' );
+	});
 	if (initial_status !== true) {
 		$('.mt-ticket-form').hide();
 		$('.mt-ticket-data input').removeAttr('required').removeAttr('aria-required');
@@ -14,31 +19,37 @@ jQuery(document).ready(function ($) {
 			$('.mt-available-tickets input').removeAttr('required').removeAttr('aria-required');
 			$('.mt-ticket-dates').hide();
 		}
-		$('.mt-trigger').on('click', function () {
-			var checked_status = $(this).prop('checked');
-
-			if (checked_status == true) {
-				$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
-				$('.mt-available-tickets input').attr('required', 'required').attr('aria-required', 'true');
-				$('.mt-ticket-form').show(300);
-			} else {
-				$('.mt-ticket-data input').removeAttr('required').removeAttr('aria-required');
-				$('.mt-ticket-form').hide(200);
-			}
-		});
-		$('input[name=mt_general]').on('change', function () {
-			var checked_status = $('input[name=mt_general]:checked').val();
-			if (checked_status == 'general') {
-				$('.mt-ticket-dates').hide();
-				$('.mt-ticket-dates input').removeAttr('required').removeAttr('aria-required');
-				$('.mt-available-tickets input').removeAttr('required').removeAttr('aria-required');
-				$('.mt-ticket-validity').show();
-			} else {
-				$('.mt-ticket-dates').show();
-				$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
-				$('.mt-available-tickets input').attr('required', 'required').attr('aria-required', 'true');
-				$('.mt-ticket-validity').hide();
-			}
-		});
 	}
+	$('.mt-trigger').on('click', function () {
+		var checked_status = $(this).prop('checked');
+		if ( ! admissionSelected ) {
+			$( '#mt-general-dated').prop( 'checked', true );
+			$('.mt-ticket-dates').show();
+			$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-available-tickets input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-ticket-validity').hide();
+		}
+		if (checked_status == true) {
+			$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-available-tickets input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-ticket-form').show(300);
+		} else {
+			$('.mt-ticket-data input').removeAttr('required').removeAttr('aria-required');
+			$('.mt-ticket-form').hide(200);
+		}
+	});
+	$('input[name=mt_general]').on('change', function () {
+		var checked_status = $('input[name=mt_general]:checked').val();
+		if (checked_status == 'general') {
+			$('.mt-ticket-dates').hide();
+			$('.mt-ticket-dates input').removeAttr('required').removeAttr('aria-required');
+			$('.mt-available-tickets input').removeAttr('required').removeAttr('aria-required');
+			$('.mt-ticket-validity').show();
+		} else {
+			$('.mt-ticket-dates').show();
+			$('.mt-ticket-dates input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-available-tickets input').attr('required', 'required').attr('aria-required', 'true');
+			$('.mt-ticket-validity').hide();
+		}
+	});
 });
