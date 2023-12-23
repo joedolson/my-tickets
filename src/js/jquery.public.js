d30408b444356372682ca4494bca173bd03a4f40
Joe Dolson <design@joedolson.com>
2023-12-23 15:01:12 -0600

    Docs update

--- src/js/jquery.public.js
+++ src/js/jquery.public.js
@@ -259,10 +259,9 @@ window.addEventListener( 'beforeunload', function(e) {
 	var unsubmitted = document.getElementById( 'mt_unsubmitted' );
 	var hold        = ( typeof( unsubmitted ) != 'undefined' && unsubmitted != null ) ? true : false;
 	if ( hold ) {
-		//following two lines will cause the browser to ask the user if they
-		//want to leave. The text of this dialog is controlled by the browser.
-		e.preventDefault(); //per the standard
-		e.returnValue = ''; //required for Chrome
+		// following lines cause the browser to ask the user if they want to leave.
+		// The text of this dialog is controlled by the browser.
+		e.preventDefault(); // per the standard
+		e.returnValue = ''; // required for Chrome
 	}
-	//else: user is allowed to leave without a warning dialog
 });
