aa258503d054e07f444f9167a56edb2b9373147d
Joe Dolson <design@joedolson.com>
2024-04-26 23:18:09 -0500

    Tweak JS

--- src/js/report.js
+++ src/js/report.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function ($) {
-    var cols = $( 'th[scope=col]' );
+    const cols = $( 'th[scope=col]' );
     $( '.show-button' ).attr( 'disabled', 'disabled' );
 
     cols.each( function() {
-        var context = $( this ).attr( 'class' );
-        var target  = $( this ).attr( 'id' );
+        let context = $( this ).attr( 'class' );
+        let target  = $( this ).attr( 'id' );
         $( this ).append( '<button type="button" class="mt-hide-button" data-context="' + context + '" aria-describedby="' + target + '"><span class="dashicons dashicons-visibility" aria-hidden="true"></span><span class="screen-reader-text">' + mtprint.mt_action_text + '</span></button>' );
     });
 
     $( 'button' ).on( 'click', function(e) {
-      var effects = $(this).attr( 'data-context' );
-	  var pressed = $(this).attr( 'aria-pressed' );
+      let effects = $(this).attr( 'data-context' );
+	  let pressed = $(this).attr( 'aria-pressed' );
 	  if ( 'true' === pressed ) {
 		$( '.' + effects ).removeClass( 'mt-hidden' );
 		$( this ).find( 'span' ).removeClass( 'dashicons-hidden' ).addClass( 'dashicons-visibility' );
