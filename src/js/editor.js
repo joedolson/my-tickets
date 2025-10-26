72d69304c6d8e319cdc461fc57bba8c71677643a
Joe Dolson <design@joedolson.com>
2025-10-26 16:24:09 -0500

    Remove unneeded comma in object.

--- src/js/editor.js
+++ src/js/editor.js
@@ -3,7 +3,7 @@
         wp.i18n.__( 'This is a required page for My Tickets. You can modify it or rename it. If you delete it, you will need to assign a new page to replace it in the My Tickets settings.', 'my-tickets' ),
         {
             id: 'mt-required-page-notice',
-            isDismissible: false,
+            isDismissible: false
         }
     );
 })(window.wp);
